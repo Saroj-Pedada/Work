affb120acbad1e56358fd1dcd46c048b477bd04a
Saroj Pedada <sarojpedada@gmail.com>
2024-04-15 16:18:42 +0530

    Shri Chatrapati Shahu Maharaj Multipurpose Organisation Ver 1.0

--- frontend/src/Components/User/Camps.jsx
+++ frontend/src/Components/User/Camps.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import HttpnInstance from "../Api/nodeapi";
 import { Carousel } from "@material-tailwind/react";
+import LoadinAnim from "../Common/LoadinAnim";
 
 function Camps(props) {
   const [campData, setCampData] = useState([]);
@@ -23,56 +24,60 @@ function Camps(props) {
     setLoading(false);
   }, [campData]);
 
-  return (
-    <div className="h-full w-full">
-      <div className="lg:my-1 flex flex-col">
-        <select
-          className="mx-auto lg:my-4 md:my-2 sm:my-1 w-fit"
-          value={selectedVillage}
-          onChange={(e) => setSelectedVillage(e.target.value)}
-        >
-          <option value="">Select Village</option>
-          {villages.map((village) => (
-            <option key={village} value={village}>
-              {village}
-            </option>
-          ))}
-        </select>
-        <div className="grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 gap-4">
-          {campData
-            .filter((item) => item.Village === selectedVillage)
-            .map((item) => (
-              <div
-                className="bg-white m-4 p-4 h-fit rounded-xl flex flex-col gap-y-2"
-                key={item._id}
-              >
-                <h2 className="text-xl font-semibold">{item.Name}</h2>
-                <Carousel className="rounded-xl h-64">
-                  {item &&
-                    item.Images &&
-                    typeof item.Images === "string" &&
-                    item.Images.split(",").map((image, index) => (
-                      <img
-                        key={index}
-                        src={image.trim()}
-                        alt={item.Name}
-                        className="w-full h-full object-cover"
-                      />
-                    ))}
-                </Carousel>
-                <p className="text-sm">Location: {item.Venue}</p>
-                <p className="text-sm">
-                  Date:{" "}
-                  {new Date(
-                    item.Date * 24 * 60 * 60 * 1000
-                  ).toLocaleDateString()}
-                </p>
-                {/* <p>{item.Village}</p> */}
-              </div>
+  return loading ? (
+    <LoadinAnim />
+  ) : (
+    <>
+      <div className="h-full w-full">
+        <div className="lg:my-1 flex flex-col">
+          <select
+            className="mx-auto lg:my-4 md:my-2 sm:my-1 w-fit"
+            value={selectedVillage}
+            onChange={(e) => setSelectedVillage(e.target.value)}
+          >
+            <option value="">Select Village</option>
+            {villages.map((village) => (
+              <option key={village} value={village}>
+                {village}
+              </option>
             ))}
+          </select>
+          <div className="grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 gap-4">
+            {campData
+              .filter((item) => item.Village === selectedVillage)
+              .map((item) => (
+                <div
+                  className="bg-white m-4 p-4 h-fit rounded-xl flex flex-col gap-y-2"
+                  key={item._id}
+                >
+                  <h2 className="text-xl font-semibold">{item.Name}</h2>
+                  <Carousel className="rounded-xl h-64">
+                    {item &&
+                      item.Images &&
+                      typeof item.Images === "string" &&
+                      item.Images.split(",").map((image, index) => (
+                        <img
+                          key={index}
+                          src={image.trim()}
+                          alt={item.Name}
+                          className="w-full h-full object-cover"
+                        />
+                      ))}
+                  </Carousel>
+                  <p className="text-sm">Location: {item.Venue}</p>
+                  <p className="text-sm">
+                    Date:{" "}
+                    {new Date(
+                      item.Date * 24 * 60 * 60 * 1000
+                    ).toLocaleDateString()}
+                  </p>
+                  {/* <p>{item.Village}</p> */}
+                </div>
+              ))}
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
 
