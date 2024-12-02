25718779626f2dfc02eda0862106fe2172aa60e3
Saroj-Pedada <sarojpedada@gmail.com>
2024-12-03 00:21:50 +0530

    smol change

--- backend/controllers/workController.js
+++ backend/controllers/workController.js
@@ -4,29 +4,16 @@ const { getIdOfUser } = require('../utils/token');
 const registerWork = (req, res) => {
     const cookies = req.body.cookies;
     const user_id = getIdOfUser(cookies);
-    const query = {
-        text: 'SELECT * FROM users WHERE id = $1',
-        values: [user_id]
+    const newquery = {
+        text: 'INSERT INTO work (id, emp_id, name, phone, village, president_name, president_phone, cards, dateofregistration, active_status, user_id) VALUES (DEFAULT, $1, $2, $3, $4, $5, $6, $7, NOW(), TRUE, $8) RETURNING *',
+        values: [req.body.emp_id, req.body.name, req.body.phone, req.body.village, req.body.president_name, req.body.president_phone, req.body.cards, user_id]
     }
-    pool.query(query, (err, result) => {
+    pool.query(newquery, (err, result) => {
         if (err) {
             console.error(err);
             res.status(500).send({ message: 'Internal Server Error' });
-        }
-        else {
-            const user_data = result.rows[0];
-            const newquery = {
-                text: 'INSERT INTO work (id, emp_id, name, phone, village, president_name, president_phone, cards, dateofregistration, active_status, user_id) VALUES (DEFAULT, $1, $2, $3, $4, $5, $6, $7, NOW(), TRUE, $8) RETURNING *',
-                values: [user_data.emp_id, user_data.name, user_data.phone, req.body.village, req.body.president_name, req.body.president_phone, req.body.cards, user_id]
-            }
-            pool.query(newquery, (err, result) => {
-                if (err) {
-                    console.error(err);
-                    res.status(500).send({ message: 'Internal Server Error' });
-                } else {
-                    res.status(200).send(result.rows[0]);
-                }
-            })
+        } else {
+            res.status(200).send(result.rows[0]);
         }
     })
 }
